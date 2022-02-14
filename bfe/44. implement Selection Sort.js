d73dbf255d4196c8f1036e3473ff8ab31a21b430
Anton Marianov <32925434+ua9@users.noreply.github.com>
2022-02-14 17:47:39 +0100

    Create 44. implement Selection Sort.js

---
+++ bfe/44. implement Selection Sort.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ */
+function selectionSort(arr) {
+  for(let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for(let j = i + 1; j < arr.length; j++) {
+      if(arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if(minIndex !== i) {
+      [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
+    }
+  }
+}
