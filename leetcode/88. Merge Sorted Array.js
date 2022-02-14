95c88e29e326039f29b9c0f8ec8c0e8375c4cb5d
Anton Marianov <32925434+ua9@users.noreply.github.com>
2022-02-14 17:41:17 +0100

    Create 88. Merge Sorted Array.js

---
+++ leetcode/88. Merge Sorted Array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let i = m - 1;
+    let j = n - 1;
+    let t = m + n - 1;
+    while(j >= 0) {
+        if (i < 0 || nums2[j] > nums1[i]) {
+            nums1[t] = nums2[j];
+            j--;
+        } else {
+            nums1[t] = nums1[i];
+            i--;
+        }
+        t--;
+    }
+};
